f6440f16a02bc798c97fbf3418863708b3617084
Jonathan Clark <jon.h.clark@gmail.com>
2012-10-01 15:19:50 -0400

    Copy required scala libs *for the current version*

--- build.sh
+++ build.sh
@@ -4,6 +4,12 @@ set -o pipefail
 set -u
 
 mkdir -p webapps/turnin/WEB-INF/classes
+scalac -version
 scalac -cp $(ls lib/*.jar webapps/turnin/WEB-INF/lib/*.jar | awk '{printf "%s:",$0}') \
     -d webapps/turnin/WEB-INF/classes \
     src/*.scala
+
+echo >&2 "Copying libraries for current scala version..."
+SCALA=$(cd $(dirname $(which scala))/..; pwd)
+cp $SCALA/lib/scala-library.jar $PWD/webapps/turnin/WEB-INF/lib/
+cp $SCALA/lib/scala-compiler.jar $PWD/webapps/turnin/WEB-INF/lib/
